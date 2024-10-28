7f4b48b48f29b96967b4b7bbe0fe2824e0220fc0
Gracie-e <callum.gracie24@gmail.com>
2024-10-28 19:41:21 +1000

    Ticket service

---
+++ Ticket_Service/Services/ITicketService.cs
@@ -0,0 +1,15 @@
+﻿using Ticket_Service.DTOs;
+using Ticket_Service.Models.Tickets;
+
+namespace Ticket_Service.Services
+{
+    public interface ITicketService
+    {
+        public Task<TicketDto> CreateTicketAsync(CreateTicketDto createTicketDto);
+        public Task<TicketDto?> GetTicketByIdAsync(int id);
+
+        public Task<IEnumerable<TicketDto>> GetSentTickets(int userId);
+        
+        public Task<IEnumerable<TicketDto>> GetReceivedTickets(int userId);
+    }
+}
