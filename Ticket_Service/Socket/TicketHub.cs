95fbc0312673d0c2261ed56ec1782075e6e8999a
Gracie-e <callum.gracie24@gmail.com>
2024-10-28 19:33:12 +1000

    ticket socket handler

---
+++ Ticket_Service/Socket/TicketHub.cs
@@ -0,0 +1,31 @@
+﻿using Ticket_Service.DTOs;
+using Ticket_Service.Services;
+
+namespace Ticket_Service.Socket;
+using Microsoft.AspNetCore.SignalR;
+
+public class TicketHub(ITicketService ticketService) : Hub
+{
+    public async Task SendTicket(CreateTicketDto createTicketDto)
+    {
+        try
+        {
+            var createdTicket = await ticketService.CreateTicketAsync(createTicketDto);
+            var receiverIds = createTicketDto.ReceiverUserIds.Select(id => id.ToString()).ToList();
+            await Clients.Users(receiverIds).SendAsync("ReceiveTicket", createdTicket);
+            await Clients.User(createTicketDto.SenderUserId.ToString()).SendAsync("TicketSent", createdTicket.Id);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            await Clients.Caller.SendAsync("TicketCreationFailed", ex.Message);
+        }
+    }
+
+    public async Task ReceiveUserTickets(int userId)
+    {
+        var userTickets = await ticketService.GetReceivedTickets(userId);
+        await Clients.Caller.SendAsync("UserTicketsReceived", userTickets);
+    }
+}
+
