53c64bafc72df0367dc5f80bc5fb627d342cbe30
Gracie-e <callum.gracie24@gmail.com>
2024-11-01 21:23:11 +1000

    Resolve incorrect path for namespace

--- Ticket_Service/Infrastructure/Data/Migrations/ApplicationDbContextModelSnapshot.cs
+++ Ticket_Service/Infrastructure/Data/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
-using Ticket_Service.Data;
+using Ticket_Service.Infrastructure.Data;
 
 #nullable disable
 
